82353d6a77d6279630b3506a297dfa32f78f36e9
Thais Vieira <thaisrvieira19@gmail.com>
2026-07-10 10:39:13 -0300

    adicionado esqueleto do projeto

---
+++ src/main/java/com/javanauta/bff_agendadortarefas/BffAgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.bff_agendadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/bff_agendadortarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.bff_agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
