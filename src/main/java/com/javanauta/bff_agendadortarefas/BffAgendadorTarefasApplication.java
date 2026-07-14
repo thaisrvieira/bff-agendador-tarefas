a25ba33fccb42388981513b4ba7b5e602c37e562
Thais Vieira <thaisrvieira19@gmail.com>
2026-07-14 14:25:41 -0300

    adicionado cron

--- src/main/java/com/javanauta/bff_agendadortarefas/BffAgendadorTarefasApplication.java
+++ src/main/java/com/javanauta/bff_agendadortarefas/BffAgendadorTarefasApplication.java
@@ -3,9 +3,11 @@ package com.javanauta.bff_agendadortarefas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 
 public class BffAgendadorTarefasApplication {
 
