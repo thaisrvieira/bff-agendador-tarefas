6c7cc74c614ccd5963aded9ff58c38373e676a20
Thais Vieira <thaisrvieira19@gmail.com>
2026-07-18 17:18:06 -0300

    finalizando dockerfile e ajustes de infraestrutura

---
+++ src/main/java/com/javanauta/bff_agendadortarefas/infrastructure/client/config/FeignError.java
@@ -0,0 +1,26 @@
+package com.javanauta.bff_agendadortarefas.infrastructure.client.config;
+
+import com.javanauta.bff_agendadortarefas.infrastructure.exceptions.BusinessException;
+import com.javanauta.bff_agendadortarefas.infrastructure.exceptions.ConflictException;
+import com.javanauta.bff_agendadortarefas.infrastructure.exceptions.ResourceNotFoundException;
+import com.javanauta.bff_agendadortarefas.infrastructure.exceptions.UnauthorizedException;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+
+public class FeignError implements ErrorDecoder {
+
+    @Override
+    public Exception decode(String s, Response response) {
+
+        switch (response.status()) {
+            case 409:
+                return new ConflictException("Erro atributo já existente ");
+            case 403:
+                return new ResourceNotFoundException("Erro atributo não encontrado ");
+            case 401:
+                return new UnauthorizedException("Erro usuário não autorizado ");
+            default:
+                return new BusinessException("Erro de servidor");
+        }
+    }
+}
---
+++ src/main/java/com/javanauta/bff_agendadortarefas/infrastructure/exceptions/BusinessException.java
@@ -0,0 +1,12 @@
+package com.javanauta.bff_agendadortarefas.infrastructure.exceptions;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String mensagem) {
+        super(mensagem);
+    }
+
+    public BusinessException(String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+}
